a8558bad7dcb0198e67fe308351dccf4dcea04d7
ilanarubin <ilanarubin007@gmail.com>
2018-07-12 21:12:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+var driver = {name: 'Sam', address: '11 Broadway'}
+function updateDriverWithKeyAndValue(driver, key, value){
+  var newDriver = Object.assign({},driver, {[key]:value})
+  return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key]= value
+  return driver; 
+}
